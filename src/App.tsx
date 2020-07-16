a8ec07c39113d2925b71dacdcc3dd489b55a3d68
Coyeah <Coyeah_chen@outlook.com>
2020-07-16 15:19:19 +0800

    updated: lodash 引用

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
-import React, { useState } from 'react';
-import _ from 'lodash-es';
-// import _ from '/web_modules/lodash-es';
+import React from 'react';
 import logo from './logo.png';
 import './index.css';
 
 export default () => {
-  const [number] = useState(null);
-
   return (
     <>
       <div className="layout">
         <div className="title">Hello World!</div>
         <div className="desc">snowpack 初尝试，学习和实践；</div>
         <img src={logo} />
-      </div>
-      <div>
-        <div>{_.isNull(number) ? '--' : number}</div>
       </div>
     </>
   );
