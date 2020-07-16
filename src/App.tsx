f27f407562a9157dc600478fd419f2be63bc1516
Coyeah <Coyeah_chen@outlook.com>
2020-07-16 11:02:20 +0800

    fix: lint error

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+import React from 'react';
 // import _ from 'lodash-es';
 import logo from './logo.png';
 import './index.css';
 
 export default () => {
-  const [number] = useState(null);
+  // const [number] = useState(null);
 
   return (
     <>
